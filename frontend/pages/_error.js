72871224ea04c99dd5aa636c3eaacdabd5299145
turisap <kirillshakirov57@gmail.com>
2019-05-07 21:22:35 +1200

    Grids finish

--- frontend/pages/_error.js
+++ frontend/pages/_error.js
@@ -1,22 +1,22 @@
 import React from "react";
 
 class Error extends React.Component {
-    /* eslint-disable react/prop-types */
-    static getInitialProps({ res, err }) {
-        const statusCode = res ? res.statusCode : err ? err.statusCode : null;
-        return { statusCode };
-    }
+  /* eslint-disable react/prop-types */
+  static getInitialProps({ res, err }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    return { statusCode };
+  }
 
-    render() {
-        return (
-          <p>
-              {this.props.statusCode
-                ? `An error ${this.props.statusCode} occurred on server`
-                : "An error occurred on client"}
-          </p>
-        );
-    }
-    /* eslint-enable react/prop-types */
+  render() {
+    return (
+      <p>
+        {this.props.statusCode
+          ? `An error ${this.props.statusCode} occurred on server`
+          : "An error occurred on client"}
+      </p>
+    );
+  }
+  /* eslint-enable react/prop-types */
 }
 
 export default Error;
