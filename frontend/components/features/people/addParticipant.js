3b19f4347e19cb5a3f7177d7566f8afe3f89b1f6
turisap <kirillshakirov57@gmail.com>
2019-05-26 21:55:31 +1200

    Add participant layout

---
+++ frontend/components/features/people/addParticipant.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import { Mutation } from "react-apollo";
+// import gql from "graphql-tag";
+// import { adopt } from "react-adopt";
+// import { CreateWithFilesUpload } from "../../abstractions/CreateWithFilesUpload";
+
+class AddParticipant extends Component {
+  render() {
+    return (
+      <p>Add participant form</p>
+    )
+  }
+}
+
+export default AddParticipant;
---
+++ frontend/pages/addParticipant.js
@@ -0,0 +1,6 @@
+import React  from "react";
+import AddParticipant from "../components/features/people/addParticipant";
+
+const addParticipantPage = () => <AddParticipant />;
+
+export default addParticipantPage;
