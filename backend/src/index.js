0d2479d28ef13c799023ff2fa15f88ea3e03baa3
turisap <kirillshakirov57@gmail.com>
2019-07-11 04:55:13 -0400

    CORS BACK

--- backend/src/index.js
+++ backend/src/index.js
@@ -53,10 +53,10 @@ server.express.use(async (req, res, next) => {
 
 
 server.start({
-    // cors : {
-    //     credentials : true,
-    //     origin : process.env.FRONTEND_URL
-    // }
+    cors : {
+        credentials : true,
+        origin : process.env.FRONTEND_URL
+    }
 }, deets => {
     /* eslint-disable no-console */
     console.log(`Server is now running on port http://localhost${deets.port}`);
