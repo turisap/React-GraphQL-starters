03e7ef7ee7206c95630d2326836f06f11e8ec686
turisap <kirillshakirov57@gmail.com>
2019-07-15 03:44:53 -0400

    Fixed bug on failed authorisation for new deploys

--- frontend/components/Auth.js
+++ frontend/components/Auth.js
@@ -6,28 +6,31 @@ import { CURRENT_USER_QUERY } from "./User";
 import SignIn from "./SignIn";
 
 const Authentication = props => (
-  <Query query={CURRENT_USER_QUERY}>
-    {({ data, loading }) => {
-      if (loading) return <p>Loading...</p>;
-      if (!data) {
-        return (
-          <div className="auth">
-            <h2 className="signInForm__heading">We need to recognize you first..</h2>
-            <SignIn />
-            <p>
-              <span>Or sign up if you are new to us</span>
-              <Link href={"/signup"}>
-                <a className="signup__button">SIGNUP</a>
-              </Link>
-            </p>
-          </div>
-        );
-      }
-      if (!data.me.emailVerified) return <p>Please verify your email</p>;
+    <Query query={CURRENT_USER_QUERY}>
+      {({ data, loading }) => {
+        if (loading) return <p>Loading...</p>;
+        if (!data.me) {
+          return (
+              <div className="auth">
+                <h2 className="signInForm__heading">
+                  We need to recognize you first..
+                </h2>
+                <SignIn />
+                <p>
+                  <span>Or sign up if you are new to us</span>
+                  <Link href={"/signup"}>
+                    <a className="signup__button">SIGNUP</a>
+                  </Link>
+                </p>
+              </div>
+          );
+        }
+        if (data.me && !data.me.emailVerified)
+          return <p>Please verify your email</p>;
 
-      return props.children;
-    }}
-  </Query>
+        return props.children;
+      }}
+    </Query>
 );
 
 Authentication.propTypes = {
